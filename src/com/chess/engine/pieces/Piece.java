5ae58e562d98864907fecaedbdc33af7d06d7304
Bilal Molani <bilalmolani99@gmail.com>
2020-11-24 22:03:17 -0500

    builder in progress

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -20,6 +20,10 @@ public abstract class Piece {
 
     }
 
+    public int getPiecePosition() {
+        return this.piecePosition;
+    }
+
     public Alliance getPieceAlliance() {
         return this.pieceAlliance;
     }
